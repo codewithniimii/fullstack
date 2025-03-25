35b25a93eaf9a37a65b46f1cae4e18a9cae0af11
Dipa Gaire <dgaire45@gmail.com>
2025-03-25 23:38:18 +0200

    made some modifications in call by reference

--- array/reference.js
+++ array/reference.js
@@ -2,4 +2,24 @@ const myProfile = {
     name: 'Nimisha'
 };
 
-console.log(myProfile.name);
+const secondProfile= {
+    name:'John',
+    age: 42
+}
+
+function primitiveMutate(primitive){
+    primitive++
+    console.log(primitive)
+}
+
+
+function mutate(obj) {
+    obj.age++
+}
+
+let num=100
+primitiveMutate(num)
+console.log(num)
+
+mutate(secondProfile)
+console.log(secondProfile)
