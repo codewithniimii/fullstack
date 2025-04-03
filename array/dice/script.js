e3a234c7061185eb267a1f09e99a2a20832ed2fb
Dipa Gaire <dgaire45@gmail.com>
2025-04-03 22:32:19 +0300

    dice game

--- array/dice/script.js
+++ array/dice/script.js
@@ -1,11 +1,53 @@
-let dice1 = document.querySelector(".img1");
-let dice2 = document.querySelector(".img2");c
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
-const randomNumber1 = Math.floor(Math.random() * 6) +1;
-const randomNumber2 = Math.floor(Math.random() * 6) +1 ;
+var randomDiceImage = "dice" + randomNumber1 + ".png";
 
-let diceImage1 = "images/dice" + randomNumber1 + ".png"; 
-let diceImage2 = "images/dice" + randomNumber2 + ".png";
+var randomImageSource = "images/" + randomDiceImage;
 
-dice1.setAttribute("src", diceImage1);
-dice2.setAttribute("src", diceImage2);
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+var randomNumber2 = Math.floor(Math.random() * 6) +1;
+
+var randomImageSource2= "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img") [1].setAttribute("src", randomImageSource2);
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 wins"
+}
+
+else if ( randomNumber2 > randomNumber1){
+    document.querySelector("h1").innerHTML= "Player 2 wins";
+}
+
+else {
+    document.querySelector("h1").innerHTML="draw";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
